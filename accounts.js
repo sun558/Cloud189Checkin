efcba507eb2478c75f24239d95176c1f37d3642c
sun558 <78543028+sun558@users.noreply.github.com>
2025-04-02 15:36:12 +0800

    Update accounts.js

--- accounts.js
+++ accounts.js
@@ -1,1 +1,7 @@
-module.exports = JSON.parse(process.env.TY_ACCOUNTS || "[]");
+module.exports = process.env.TY_ACCOUNTS 
+  ? process.env.TY_ACCOUNTS
+      .split('\n')          // 按行分割
+      .map(line => line.trim()) // 去除每行首尾空格
+      .filter(Boolean)      // 移除空行
+  : [];
+
